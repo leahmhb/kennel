674c13a219032b7dc68e8eff2024c3bdc6bbed4d
Leah B <leah.mhb@gmail.com>
2020-04-06 15:41:21 -0400

    added header links and bootstrap4 to vue project

--- vue-cli/src/components/Header.vue
+++ vue-cli/src/components/Header.vue
@@ -1,3 +1,37 @@
 <template>
-<h1>Header</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<router-link class="navbar-brand" to="/">Stock Trader</router-link>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+<ul class="navbar-nav mr-auto">
+<router-link class="nav-link" to="/portfolio" activeClass="active" tag="li">
+<a>Portfolio <span class="sr-only">(current)</span></a>
+</router-link>
+<router-link class="nav-link" to="/stocks" activeClass="active" tag="li">
+<a>Stocks</a>
+</router-link>
+</ul>
+
+<ul class="navbar-nav">
+    <li class="nav-item"><a class="nav-link" href="#">End Day</a></li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" 
+        href="#" 
+        id="navbarDropdown" 
+        role="button" 
+        data-toggle="dropdown" 
+        aria-haspopup="true" 
+        aria-expanded="false">
+        Save & Load
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Save</a>
+            <a class="dropdown-item" href="#">Load</a>
+        </div>
+    </li>
+</ul>
+
+</div>
+</nav>
 </template>
